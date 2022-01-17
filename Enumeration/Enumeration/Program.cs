dce2934211c7405d86f38873c09817d269cfa0e3
imeroboma <84933908+imeroboma@users.noreply.github.com>
2022-01-17 13:38:01 -0700

    Add files via upload

---
+++ Enumeration/Enumeration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enumeration
+{
+    // enumeration is a type definition
+    enum season { Spring, Summer, Fall, Winter};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NR_SEASONS = 4;
+            decimal[] sales = new decimal[NR_SEASONS];
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
