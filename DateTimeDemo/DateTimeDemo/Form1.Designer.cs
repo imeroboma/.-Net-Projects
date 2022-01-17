81d0e7aef8e2b9f848d1876a7a1841f587359996
imeroboma <84933908+imeroboma@users.noreply.github.com>
2022-01-17 13:36:08 -0700

    Add files via upload

---
+++ DateTimeDemo/DateTimeDemo/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace DateTimeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDate.Location = new System.Drawing.Point(293, 13);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(100, 23);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDate.Click += new System.EventHandler(this.lblDate_Click);
+            // 
+            // lbltime
+            // 
+            this.lbltime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbltime.Location = new System.Drawing.Point(293, 72);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(100, 23);
+            this.lbltime.TabIndex = 1;
+            this.lbltime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 81);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "label1";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(70, 74);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 289);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.lblDate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ DateTimeDemo/DateTimeDemo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDate_Click(object sender, EventArgs e) => Display();
+
+        private void Display()
+        {
+            lblDate.Text = DateTime.Now.ToLongDateString(); //just the date
+            lbltime.Text = DateTime.Now.ToShortTimeString(); //only time
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Display();
+        }
+          //user selected date
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime partyDay = dateTimePicker1.Value.Date; //date only
+            //how many days to party
+            TimeSpan difference = partyDay.Subtract(DateTime.Today);
+            //lblMessage.Text = "The party is on" + partyDay.ToLongDateString();
+            //partyDay.ToLongDateString();
+            lblMessage.Text = $"The party is on {partyDay.DayOfWeek}, {partyDay.ToLongDateString()}";
+        }
+    }
+}
